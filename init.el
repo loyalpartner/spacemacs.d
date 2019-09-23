2264fb6d098051da984a466f6abf49a343c9b604
凯 <loyalpartner@163.com>
2019-09-23 21:03:28 +0800

    补全的时候显示序号。

--- init.el
+++ init.el
@@ -487,6 +487,7 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (setq company-show-numbers t)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
